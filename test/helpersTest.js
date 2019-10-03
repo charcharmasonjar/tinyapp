dbeac877b8d99de4e8bab5b3d65597b85989b661
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-03 14:24:08 -0700

    Create tests for urlsForUser function

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers.js');
+const { getUserByEmail, urlsForUser } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -15,15 +15,38 @@ const testUsers = {
   }
 };
 
+const testDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "userRandomID" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "user2RandomID" },
+  sm5xKQ: { longURL: "https://www.youtube.com", userID: "userRandomID" }
+};
+
 describe('getUserByEmail', function() {
+
   it('should return a user with valid email', function() {
     const userObj = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = testUsers["userRandomID"];
-    console.log(userObj);
-    console.log(expectedOutput);
     assert.deepEqual(userObj, expectedOutput);
   });
+
   it('should return undefined if email does not exist', function() {
     assert.equal(getUserByEmail("yeet@yahoo.ca", testUsers), undefined);
+  });
+});
+
+describe('urlsForUser', function() {
+
+  it('should return a list of URLs when given a user ID', function() {
+    const actual = urlsForUser("userRandomID", testDatabase);
+    const expected = {
+      b6UTxQ: "https://www.tsn.ca",
+      sm5xKQ: "https://www.youtube.com",
+    };
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should return an empty object if userID does not exist in user database', function() {
+    const actual = urlsForUser("succulent", testDatabase);
+    assert.deepEqual(actual, {});
   });
 });
