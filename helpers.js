9713e3f5d8a613496050e77bf9de372e8ce5cfc2
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-03 20:47:47 +0000

    Clean up spacing and semicolons

--- helpers.js
+++ helpers.js
@@ -1,12 +1,12 @@
 //checks if email already exists in a given object
 //if email exists, returns the user object that contains the email
-const getUserByEmail = function (email, database) {
+const getUserByEmail = function(email, database) {
   for (let id in database) {
     if (database[id].email === email) {
       return database[id];
     }
   }
   return undefined;
-}
+};
 
 module.exports = { getUserByEmail };
